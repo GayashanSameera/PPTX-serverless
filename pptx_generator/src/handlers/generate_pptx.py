30e8b71994bf1db8e01a6a83a18f7480fa9bada3
hacSandamali <49017862+hacSandamali@users.noreply.github.com>
2022-07-31 13:01:15 +0530

    Text replace chages

--- pptx_generator/src/handlers/generate_pptx.py
+++ pptx_generator/src/handlers/generate_pptx.py
@@ -7,6 +7,19 @@ from copy import deepcopy
 from pptx.table import Table, _Row, _Column, _Cell
 from pptx.enum.text import PP_ALIGN
 import re
+import enum
+
+class CommandRegex(enum.Enum):
+    TEXT = r'\+\+\+INS (.*?) \+\+\+'
+
+class Command(enum.Enum):
+    IF_CONDITION = "if_condition"
+    FOR_LOOP = "for_loop"
+    REPLACE_IMAGE = "replace_image"
+    REPLACE_TABLE = "replace_table"
+    UPDATE_TABLE_TEXT = "update_table_text"
+    DRAW_TABLE = "draw_tables"
+    TEXT_REPLACE = "replace_text"
 
 # Parent Tag class
 class Tag:
@@ -34,7 +47,7 @@ class Tag:
                     new_text = cell.text.replace(replaced_for, replaced_text)
                     cell.text = new_text
  
-    def get_object_values():
+    def get_object_values(pattern, shape):
         matches = super().get_tag_content(pattern,shape)
         if( not matches or len(matches) < 1):
              return
@@ -46,7 +59,18 @@ class Tag:
         matches = re.findall(pattern, string)
         return matches
     
-    
+    def text_tag_update(text, pattern):
+        current_text = text
+        matches = self.get_tag_from_string(pattern, text)
+        if( not matches or len(matches) < 1):
+            return { "text": text }
+
+        for match in matches:
+            object_value = pydash.get(replacements, match, False)
+            if(object_value != False):
+                current_text = current_text.replace(str(f"+++INS {match} +++"), str(object_value))
+        
+        return { "text": current_text } 
 
 class Image(Tag):
     def __init__(self):
@@ -69,5 +93,50 @@ class Image(Tag):
         
             slide.shapes.add_picture(url, Inches(left), Inches(top), Inches(width) ,Inches(height) )
             super().replace_tags(str(f"+++IM {match} +++"), "", shape)
-            
+
+class Text(Tag):
+    def __init__(self):
+        super().__ini__()
+        self.pattern = CommandRegex.TEXT
+
+    def replace_text(slide, shape, pattern):
+        match, object_value = super().get_object_values(pattern,shape)
+        super().replace_tags(str(f"+++INS {match} +++"), str(object_value), shape)  
+
+class Expression(Tag):
+    def __init__(self):
+        super().__init__()
+
+    def if_condition(self):
+        print("if condition func executed")
+
+    def for_loop(self):
+        print("for loop func executed")
+        
+class CommandRegistry:
+    def __init__(self):
+        self.commands = {
+            "+++IF": "if_condition",
+            "+++FOR": "for_loop",
+            "+++IM": "replace_image",
+            "+++TB_ADD": "replace_table",
+            "+++TB_TX_UP": "update_table_text",
+            "+++TB_DRW": "draw_tables",
+            "+++INS": "replace_text"
+        }
     
+    def get_commands_dictionary(self):
+        return self.commands
+
+    def get_commands_list(self):
+        return self.commands.keys()
+
+    def get_command(self, command_name):
+        if command_name == Command.TEXT_REPLACE:
+            text = Text()
+            text.replace_text()
+        elif command_name == Command.FOR_LOOP:
+            expression = Expression()
+            return expression.for_loop
+        else:
+            raise Exception("Invalid command name")
