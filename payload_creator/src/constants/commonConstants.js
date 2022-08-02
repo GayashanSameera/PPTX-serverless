7ac3d4a63448d19a1891510ff0efd63d4bb90172
Ahmed-Akeel <It20129958@my.sliit.lk>
2022-08-02 17:38:52 +0530

    Fetch Data Implementation

---
+++ payload_creator/src/constants/commonConstants.js
@@ -0,0 +1,5 @@
+const B2B_AUTH=''
+
+export default commonConstants={
+    B2B_AUTH,
+}
--- payload_creator/src/helpers/index.js
+++ payload_creator/src/helpers/index.js
@@ -1,7 +1,9 @@
 
 import responseHelper from "./responseHelper"
 import lambdaHelper from "./lamdaHelper";
+
 export {
    responseHelper,
    lambdaHelper,
+   
 };
